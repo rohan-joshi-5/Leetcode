1cd127aa6a25534141cc54354c9216dfbf5f929b
rohanjoshi <joshirohan2005@gmail.com>
2025-07-30 04:36:34 +0000

    Code challenge submission for question: 1431 Kids With the Greatest Number of Candies

---
+++ 1431 Kids With the Greatest Number of Candies/1431kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max=*max_element(candies.begin(),candies.end());
+        vector<bool> result;
+        for(auto i:candies)
+        {
+            if(i+extraCandies>=max)
+            result.push_back(true);
+            else
+            result.push_back(false);
+        }
+        return result;
+    }
+};
