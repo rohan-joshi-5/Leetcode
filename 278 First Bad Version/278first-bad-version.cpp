dc7cab5b17ecb754f474ff99db5085beae6268d7
rohanjoshi <joshirohan2005@gmail.com>
2025-08-06 01:46:27 +0000

    Code challenge submission for question: 278 First Bad Version

---
+++ 278 First Bad Version/278first-bad-version.cpp
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long left=1;
+        long long right=n;
+        while(left<=right){
+            long long mid=left+(right-left)/2;
+            if(isBadVersion(mid)){
+                right=mid-1;
+            }
+            else left=mid+1;
+        }
+        return right+1;
+    }
+};
