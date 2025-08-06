d42640a0807d36f0ebf757030fe0c6912ba87f88
rohanjoshi <joshirohan2005@gmail.com>
2025-08-06 15:31:10 +0000

    Code challenge submission for question: 744 Find Smallest Letter Greater Than Target

---
+++ 744 Find Smallest Letter Greater Than Target/744find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int l=0;
+        int r=letters.size()-1;
+        int mid,pos=-1; 
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(letters[mid]>target){
+                pos=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            };
+        }
+        if(pos==-1)
+            return letters[0];
+        return letters[pos];
+    }
+};
