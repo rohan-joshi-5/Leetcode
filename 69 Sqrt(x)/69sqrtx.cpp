3a7c3217eac553250a76cea1fa39ddd1adc62b38
rohanjoshi <joshirohan2005@gmail.com>
2025-09-03 07:33:13 +0000

    Code challenge submission for question: 69 Sqrt(x)

---
+++ 69 Sqrt(x)/69sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left=0,right=x;
+        int ans=0;
+        while(left<=right){
+            long long mid=left+(right-left)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if(mid*mid<x){
+                ans=mid;
+                left=mid+1;
+            }
+            else {
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+};
