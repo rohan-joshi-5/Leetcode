8cfcaa4470fd854129f7434fc1fa0181d843b501
rohanjoshi <joshirohan2005@gmail.com>
2025-09-06 16:30:32 +0000

    Code challenge submission for question: 3 Longest Substring Without Repeating Characters

---
+++ 3 Longest Substring Without Repeating Characters/3longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,21 @@
+// a b c a b c b b
+//     i
+//           j
+//3
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> seen;
+        int left=0;
+        int maxlen=0;
+        for(int right=0;right<s.size();right++){
+            while(seen.count(s[right])){
+                seen.erase(s[left]);
+                left++;
+            }
+            seen.insert(s[right]);
+            maxlen=max(maxlen,right-left+1);
+        }
+        return maxlen;
+    }
+};
