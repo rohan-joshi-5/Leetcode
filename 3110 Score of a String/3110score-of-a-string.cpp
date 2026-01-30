daf74338a5f35b3adcd2177d28edf567c0424ed2
rohanjoshi <joshirohan2005@gmail.com>
2026-01-30 05:51:09 +0000

    Code challenge submission for question: 3110 Score of a String

---
+++ 3110 Score of a String/3110score-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int n =s.size();
+        int score =0;
+        for(int i=1;i<n;i++){
+            score+=abs(s[i]-s[i-1]);
+        }
+        return score;
+    }
+};
