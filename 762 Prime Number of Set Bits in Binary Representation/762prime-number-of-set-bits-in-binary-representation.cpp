46155e65613e1b0601f7574c2ce02e0f36580959
rohanjoshi <joshirohan2005@gmail.com>
2026-02-21 10:15:35 +0000

    Code challenge submission for question: 762 Prime Number of Set Bits in Binary Representation

---
+++ 762 Prime Number of Set Bits in Binary Representation/762prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        unordered_set<int> primes={2,3,5,7,11,13,17,19};
+        int result=0;
+        for(int num=left;num<=right;num++){
+            int setbits=__builtin_popcount(num);
+            if(primes.count(setbits))
+                result+=1;
+        }
+        return result;
+    }
+};
