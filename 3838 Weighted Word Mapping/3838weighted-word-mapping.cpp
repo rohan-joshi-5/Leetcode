0d4305416858162087a289e983af3579a85173ee
rohanjoshi <joshirohan2005@gmail.com>
2026-06-13 14:50:43 +0000

    Code challenge submission for question: 3838 Weighted Word Mapping

---
+++ 3838 Weighted Word Mapping/3838weighted-word-mapping.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string result="";
+        for(string &word:words){
+            int current_weight=0;
+            for(char c:word){
+                current_weight +=weights[c-'a'];
+            }
+        int mod_val=current_weight%26;
+        result+=(char)('z'-mod_val);
+        }
+        return result;
+
+    }
+};
